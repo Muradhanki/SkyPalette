692593433d31810b5e7dd8a459785f70bf2edc32
Natalie Bo <nataliabo@hotmail.co.uk>
2024-03-17 11:58:44 +0300

    feat:body component update with added user form.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import UserInputForm from './UserInputForm'
+// import React from 'react'
+// import UserInputForm from './UserInputForm'
+
+// function Body() {
+//   return (
+//     <div>Body</div>
+//   )
+// }
+
+
+// export default Body
+
+//NEW natt5
+
+
+import React from 'react';
+import UserInputForm from './UserInputForm';
+
 
 function Body() {
   return (
-    <div>Body</div>
-  )
+    <div>
+  
+      <UserInputForm />
+    </div>
+  );
 }
 
-
-export default Body
+export default Body;
