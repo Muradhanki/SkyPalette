d89af5d23e7b7ff76c323872fb6afeb2ac009c64
Muradhanki <151674559+Muradhanki@users.noreply.github.com>
2024-03-20 22:35:00 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from ‘react’;
+import { Link } from ‘react-router-dom’;
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-orange">
-      <div className="container">
-        <Link to="/" className="navbar-brand">
-          <img src="/images/skypalette-logo.png" className="nav--icon" alt="Logo" />
-      
+    <nav className=“navbar navbar-expand-lg navbar-light bg-orange”>
+      <div className=“container”>
+        <Link to=“/” className=“navbar-brand”>
+          <img src=“/images/skypalette-logo.png” className=“nav--icon” alt=“Logo” />
         </Link>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
-          <li className="nav-item">
-              <Link to="/" className="nav-link">HOME</Link>
+        <div className=“collapse navbar-collapse justify-content-end” id=“navbarNav”>
+          <ul className=“navbar-nav”>
+          <li className=“nav-item”>
+              <Link to=“/” className=“nav-link”>HOME</Link>
             </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link">ABOUT</Link>
+            <li className=“nav-item”>
+              <Link to=“/about” className=“nav-link”>ABOUT</Link>
             </li>
-            <li className="nav-item">
-                  <Link to="/contact" className="nav-link">CONTACT</Link>
+            <li className=“nav-item”>
+                  <Link to=“/contact” className=“nav-link”>CONTACT</Link>
               </li>
           </ul>
         </div>
       </div>
-    </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">HOME</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">ABOUT</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">CONTACT</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
+    </nav>
   );
 }
-
 export default Navbar;
-
