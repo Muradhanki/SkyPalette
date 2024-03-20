d2f42aa4ea94f3fa0c482e58b98f3bcafffd99f7
KodaKing <m.maso@live.co.uk>
2024-03-20 19:16:52 +0000

    Added API based fonts the search and results containers.

--- src/components/UserInputForm.jsx
+++ src/components/UserInputForm.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { fetchWeatherData, fetchEventData } from './api'; //Functions to fetch data
-
-const HolidayPlanner = () => {
-const [datesOfTravel, setDatesOfTravel] = useState({ startDate: '', endDate: ''})
-const [climatePreference, setClimatePreference] = useState('');
-const [activities, setActivities] = useState([]);
-
-const handleSubmit = async (event) =>  {
-    event.preventDefault();
-    //Make API calls with user input
-    const weatherData = await fetchWeatherData(datesOfTravel, climatePreference);
-
-return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <label>
-Start Date:
-<input type="date" value={datesOfTravel.startDate} onChange={(e) => setDatesOfTravel({ ...datesOfTravel, startDate: e.target.value })} />
-            </label>
-            <label>
-                End Date:
-               <input type="date" value="{datesOfTravel.endDate}" onChange={(e) => setDatesOfTravel({ ...datesOfTravel, endDate: e.target.value })}/>
-            </label>
-            {/* Add more input fields for climate preference and activities */}
-        <button type="submit">Submit</button>
-            </form>
-=======
 // import React, { useState } from 'react';
 // import { fetchWeatherData, fetchEventData } from './api'; //Functions to fetch data
 
@@ -589,14 +560,9 @@ const UserInputForm = () => {
 </div>
 
       </form>
->>>>>>> 8472b9538c4946b93cb766b3ba016d1deb410ad2
     </div>
 );
 };
 
-<<<<<<< HEAD
-export default HolidayPlanner;
-=======
 export default UserInputForm;
 
->>>>>>> 8472b9538c4946b93cb766b3ba016d1deb410ad2
