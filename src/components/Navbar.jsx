d41511db05f1cbdff39afb196ba12ccd20b3f218
Natalie Bo <nataliabo@hotmail.co.uk>
2024-03-22 20:23:46 +0300

    feat:created a burger icon dropdown menu for mobile optimised view.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-orange">
       <div className="container">
         <Link to="/" className="navbar-brand">
           <img src="/images/skypalette-logo.png" className="nav--icon" alt="Logo" />
         </Link>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link to="/" className="nav-link">HOME</Link>
